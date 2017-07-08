ce5ccfc82c90455485f43437bbba927d4d3e09f5
Sivasankar Sivalingam <Sivasankar-Sivalingam@users.noreply.github.com>
2017-07-08 19:26:33 +0530

    login
    changes for login function

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 import { CookieManager } from '../../services/cookie.service'
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
 
 @Component({
 
     selector: "app-login",
     templateUrl: "./login.component.html",
     styleUrls: ['./login.component.css']
-
-
-
 })
 export class LoginComponent {
     public user: any = { username: "", password: "" };
+    public validUser: boolean;
+    constructor(private router:Router){
+        this.validUser = true;
+        }
+
+    loginUser(user){
+        var userName:string = user.username;
+        var password:string = user.password;
+        var tempName:string = 'admin';
+        var tempPwd:string = 'admin';
+        if(userName === tempName && password === tempPwd){
+            this.validUser = true;
+            this.router.navigate(['Tracker']);
+        }else{
+            this.validUser = false;
+        }
+    }
 }
